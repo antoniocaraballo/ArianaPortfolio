9dbd590f037eb2f0d7a98f412a2a58dbba04a206
antoniocaraballo <antonio@MacBook-Pro.attlocal.net>
2022-08-22 17:09:15 -0400

    Mobile Overflow x fix

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const navMenu = document.querySelector("#nav-menu");
 
 function openNav() {
   navMenu.style.right = "0%";
+  //   navMenu.style.display = "flex";
   document.body.style.overflow = "hidden";
 }
 
 function closeNav() {
   navMenu.style.right = "-50%";
+  //   navMenu.style.display = "none";
   document.body.style.overflow = "scroll";
 }
