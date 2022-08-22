86cbc90d320dd24076ae3194df4c57ce3e818f22
antoniocaraballo <antonio@MacBook-Pro.attlocal.net>
2022-08-22 17:37:39 -0400

    Navbar mobile view

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const navMenu = document.querySelector("#nav-menu");
 
 function openNav() {
   navMenu.style.right = "0%";
-  //   navMenu.style.display = "flex";
+  navMenu.style.visibility = "visible";
   document.body.style.overflow = "hidden";
 }
 
 function closeNav() {
   navMenu.style.right = "-50%";
-  //   navMenu.style.display = "none";
+  navMenu.style.visibility = "hidden";
   document.body.style.overflow = "scroll";
 }
