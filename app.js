fe5d53179528c1132d4bfc431afbe5fd9f3c1a93
antoniocaraballo <antonio@MacBook-Pro.attlocal.net>
2022-08-22 16:49:43 -0400

    Fix overflow x bug

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ const navMenu = document.querySelector("#nav-menu");
 
 function openNav() {
   navMenu.style.right = "0%";
-  console.log("hello");
   document.body.style.overflow = "hidden";
 }
 
 function closeNav() {
   navMenu.style.right = "-50%";
-  console.log("hello");
   document.body.style.overflow = "scroll";
 }
